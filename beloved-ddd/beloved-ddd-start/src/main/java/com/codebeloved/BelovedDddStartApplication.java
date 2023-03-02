d7a2a9d2d1df3e25125a0aa94930b939a61873f5
baipengyan <baipengyan0101@qq.com>
2023-03-02 11:45:14 +0800

    Initial commit

---
+++ beloved-ddd/beloved-ddd-start/src/main/java/com/codebeloved/BelovedDddStartApplication.java
@@ -0,0 +1,14 @@
+package com.codebeloved;
+
+/**
+ * TODO
+ *
+ * @author ${USER}
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
