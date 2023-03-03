600d03df6af809607c20ec69f64f4716c56b9b75
baipengyan <baipengyan0101@qq.com>
2023-03-03 13:47:31 +0800

    feat(模块): 新增beloved-demo模块

---
+++ beloved-demo/beloved-demo-start/src/main/java/com/codebeloved/BelovedDemoStartApplication.java
@@ -0,0 +1,20 @@
+package com.codebeloved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ *
+ * @author baipengyan
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BelovedDemoStartApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BelovedDemoStartApplication.class, args);
+	}
+}
